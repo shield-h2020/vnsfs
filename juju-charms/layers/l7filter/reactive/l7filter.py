a765be831c935dff209a39adc0956e3674153aff
Carolina Fernandez <carolina.fernandez@i2cat.net>
2017-10-11 22:48:44 +0200

    L7Filter v.1, charm code refactor Signed-off-by: Carolina Fernandez <carolina.fernandez@i2cat.net>

--- juju-charms/layers/l7filter/reactive/l7filter.py
+++ juju-charms/layers/l7filter/reactive/l7filter.py
@@ -5,7 +5,7 @@ from charmhelpers.core.hookenv import (
     config,
     status_set,
 )
-#from charmhelpers.contrib.openstack.utils import get_host_ip
+
 from charms.reactive import (
     remove_state as remove_flag,
     set_state as set_flag,
@@ -19,93 +19,67 @@ from subprocess import (
     PIPE,
 )
 
-
 cfg = config()
-#host_ip = get_host_ip(unit_get("public-address"))
-rest_ip = cfg.get("rest-ip", "127.0.0.1")
-rest_port = cfg.get("rest-port", "8082")
+rest_ip = cfg.get("ssh-hostname")
+rest_port = cfg.get("rest-port")
 status_file = "l7filter_status.log"
 policies_file = "policies"
 
-@when('config.changed')
+@when("config.changed")
 def config_changed():
-    set_flag('l7filter.configured')
-    status_set('active', 'ready!')
+    set_flag("l7filter.configured")
+    status_set("active", "ready!")
     return
 
 
-@when('l7filter.configured')
-@when('actions.start')
+@when("l7filter.configured")
+@when("actions.start")
 def start():
-    try:
-        # Enter the command to start your service(s)
-        cmd = "touch ~/" + status_file + "; echo \"" + log_action("start") + "\" >> ~/" + status_file
-        result, err = charms.sshproxy._run(cmd)
-    except Exception as e:
-        err = "{}".format(e)
-        action_fail('command failed: {}, errors: {}'.format(err, e.output))
-        remove_flag('actions.start')
-        return
-
-@when('l7filter.configured')
-@when('actions.stop')
-def stop():
-    try:
-        # Enter the command to stop your service(s)
-        cmd = "echo \"" + log_action("stop") + "\" >> ~/" + status_file + "; rm ~/" + status_file
-        result, err = charms.sshproxy._run(cmd)
-    except Exception as e:
-        action_fail('command failed: {}, errors: {}'.format(e, e.output))
-    else:
-        action_set({'stdout': result,
-                    'errors': err})
-    finally:
-        remove_flag('actions.stop')
-
-@when('l7filter.configured')
-@when('actions.restart')
-def restart():
-    try:
-        # Enter the command to restart your service(s)
-        cmd = "touch ~/" + status_file + "; echo \"" + log_action("restart") + "\" >> ~/" + status_file
-        result, err = charms.sshproxy._run(cmd)
-    except Exception as e:
-        action_fail('command failed: {}, errors: {}'.format(e, e.output))
-    else:
-        action_set({'stdout': result,
-                    'errors': err})
-    finally:
-        remove_flag('actions.restart')
+    cmd = "touch ~/" + status_file + "; echo \"" + log_action("start") + "\" >> ~/" + status_file
+    ssh_call("actions.start", cmd)
 
 @when("l7filter.configured")
-@when("actions.set-policies")
-def set_policies():
-    try:
-        curl_call("actions.set-policies", "/createDB", "GET")
-    except:
-        pass
-    policies = read_policies_content(action_get('policies'))
-    policies = """{}""".format(policies)
-    headers = {"Content-Type": "application/xml"}
-    curl_call("actions.set-policies", "/createFlowsXML/v2", 
-        "POST", headers, str(policies))
+@when("actions.stop")
+def stop():
+    cmd = "touch ~/" + status_file + "; echo \"" + log_action("stop") + "\" >> ~/" + status_file
+    ssh_call("actions.stop", cmd)
+
+@when("l7filter.configured")
+@when("actions.restart")
+def restart():
+    cmd = "touch ~/" + status_file + "; echo \"" + log_action("restart") + "\" >> ~/" + status_file
+    ssh_call("actions.restart", cmd)
 
 @when("l7filter.configured")
 @when("actions.get-policies")
 def get_policies():
     headers = {"Content-Type": "application/json"}
-    curl_call("actions.get-policies", "/getFlow/v2", "POST", headers)
+    args = [("actions.get-policies", "/getFlow/v2", "POST", headers)]
+    ssh_curl_call(args)
 
 @when("l7filter.configured")
-@when("actions.delete-policy")
-def delete_policy():
-    policy = action_get('policy')
-    curl_call("actions.delete-policy", "/deleteFlow/v2?id={}".format(policy), "DELETE")
+@when("actions.set-policies")
+def set_policies():
+    policies = action_get("policies")
+    policies = read_policies_content(policies)
+    args = [("actions.set-policies", "/createDB", "GET")]
+    headers = {"Content-Type": "application/xml"}
+    args += [("actions.set-policies", "/createFlowsXML/v2",
+        "POST", headers, policies)]
+    ssh_curl_call(args)
 
 @when("l7filter.configured")
 @when("actions.delete-policies")
 def delete_policies():
-    curl_call("actions.delete-policies", "/deleteDB", "DELETE")
+    args = [("actions.delete-policies", "/deleteDB", "DELETE")]
+    ssh_curl_call(args)
+
+@when("l7filter.configured")
+@when("actions.delete-policy")
+def delete_policy():
+    policy = action_get("policy")
+    args = [("actions.delete-policy", "/deleteFlow/v2?id={}".format(policy), "DELETE")]
+    ssh_curl_call(args)
 
 
 def read_policies_content(policies):
@@ -118,10 +92,10 @@ def read_policies_content(policies):
         response = urllib.request.urlopen(policies)
         contents = response.read()
     except (HTTPError, URLError, ValueError) as e:
-        print("No valid URL")
+        print("Direct content or invalid URL provided")
     return contents
 
-def ssh_call(cmd):
+def ssh_call(action_name, cmd):
     try:
         result, err = charms.sshproxy._run(cmd)
     except Exception as e:
@@ -131,6 +105,13 @@ def ssh_call(cmd):
                     "errors": err})
     finally:
         remove_flag(action_name)
+
+def ssh_curl_call(arg_list):
+    for args in arg_list:
+        try:
+            curl_call(*args)
+        except:
+            pass
 
 def curl_call(action_name, path, method, headers={}, data={}):
     try:
@@ -142,26 +123,26 @@ def curl_call(action_name, path, method, headers={}, data={}):
                 headers=headers,
                 verify=False)
         elif method == "POST":
-            data = """{}""".format(data)
             resp = requests.post(curl_url,
                 headers=headers,
                 data=data,
                 verify=False)
         elif method == "DELETE":
             resp = requests.delete(curl_url,
-                headers=headers,
-                verify=False)
-        if resp:
-            result = resp.text
+                    headers=headers,
+                    verify=False)
+        result = resp.text
     except Exception as e:
-        action_fail("command failed: {}, errors: {}, endpoint: {}:{}".format(e, e.output, rest_ip, rest_port))
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        action_fail("command failed: {}, endpoint: {}:{}, filename: {}, line: {}".format(e, rest_ip, rest_port, fname, exc_tb.tb_lineno))
     else:
         action_set({"stdout": result,
-                    "errors": err})
+                    "errors": e})
     finally:
         remove_flag(action_name)
 
 def log_action(action):
     now = datetime.datetime.now()
-    message = "[{}] (ip={},port={}) action={}".format(now.isoformat(), rest_ip, rest_port, action)
+    message = "[{}] action={}".format(now.isoformat(), action)
     return message
