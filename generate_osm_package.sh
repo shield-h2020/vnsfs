b894d26265f62478341f0d80a5e5eeb402fb46ff
Carolina Fernandez <carolina.fernandez@i2cat.net>
2018-03-26 18:04:10 +0200

    SHIELD-133: source juju deps
    Signed-off-by: Carolina Fernandez <carolina.fernandez@i2cat.net>

--- generate_osm_package.sh
+++ generate_osm_package.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+source "juju-charms/deps.sh"
+
 v_id="$1"
 
 repo_root=$PWD
 pkg_tmp=$(mktemp -d)
 v_id_vnfd="${pkg_tmp}/descriptor-packages/vnfd/${v_id}_vnf"
 v_id_nsd="${pkg_tmp}/descriptor-packages/nsd/${v_id}_ns"
-#cp -Rp $repo_root/!(*.git*) $pkg_tmp/
 cp -Rp $repo_root/{descriptor-packages,juju-charms} $pkg_tmp/
 cd $pkg_tmp
 
@@ -18,11 +19,6 @@ error() {
 [[ -z $v_id ]] && error "The name of a vNSF must be provided"
 [[ ! -d $v_id_vnfd ]] && error "Provided vNSF has a name different to ${v_id}_vnf in the descriptor-packages folder"
 [[ ! -d $v_id_nsd ]] && error "Provided NS has a name different to ${v_id}_ns in the descriptor-packages folder"
-
-juju_in=$(dpkg -l | grep juju)
-if [[ -z $juju_in || $juju_in != *"ii"* ]]; then
-  error "Juju and charms must be installed first: sudo add-apt-repository ppa:juju/stable; sudo apt update; sudo apt install -y juju charm"
-fi
 
 clear
 
@@ -36,7 +32,6 @@ charm build
 mkdir -p ${v_id_vnfd}/charms
 cd ../../
 mv builds/${v_id} ${v_id_vnfd}/charms/
-#rm -r builds
 
 # Generate OSM package for vNSF
 cd ${pkg_tmp}/descriptor-packages/vnfd
